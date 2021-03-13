f24b8c40c1b751f10485cb8c37ff22beed6ff9cb
kramsuiluj <mjmaravillo@gmail.com>
2021-03-13 17:38:31 +0800

    added index.php file

---
+++ index.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Build-Progress</title>
+</head>
+<body>
+    
+    <div>
+    
+        <form action="">
+            <section>
+                <label for="">Department</label><br>
+                <input type="text" name="department">
+            </section>
+            <section>
+                <label for="">Project Name</label><br>
+                <input type="text" name="pname">
+            </section>
+            <section>
+                <label for="">Start Date</label><br>
+                <input type="text" name="sdate">
+            </section>
+            <section>
+                <label for="">Project Progress</label><br>
+                <input type="text" name="progress">
+            </section>
+            <section>
+                <input type="submit" name="submit" value="SUBMIT">
+            </section>
+        </form>
+    
+    </div>
+
+</body>
+</html>
