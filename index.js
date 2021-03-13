cb653e16d400a8904abcc0c7b65d88687716efbf
kramsuiluj <mjmaravillo@gmail.com>
2021-03-14 01:40:01 +0800

    added validate function in index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+// form validation script
+
+// This line of codes gets all the name of input fields and store it to a variable.
+const inputs = document.querySelectorAll('input');
+
+// This block of code stores all the regex patterns.
+const patterns = {
+    pname: /^[\w\.?-?]{4,50}$/
+}
+
+// Validation function
+function validate (field, regex) {
+
+    // This block of code tests if the user input is true or false, if its true the selected field will have a green border, otherwise it will have red.
+    if (regex.test(field.value)) {
+
+        field.className = 'input-valid';
+
+    } else {
+
+        field.className = 'input-invalid';
+
+    }
+
+}
+
+inputs.forEach((input) => {
+
+    input.addEventListener('keyup', (e) => {
+
+        validate(e.target, patterns[e.target.attributes.name.value])
+
+    });
+
+});
